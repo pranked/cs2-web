f58e4e7a29cbdcbe96542382186191cea831c98b
Dominic Moorman <dom@levee.co>
2016-09-22 13:27:45 -0500

    License

--- src/views/Components/Footer.js
+++ src/views/Components/Footer.js
@@ -4,21 +4,21 @@ import './Footer.scss';
 
 const Footer = React.createClass({
   render() {
+    const size = '35px';
     return (
       <footer className="footer">
         <div className="container">
           <div className="row">
             <div className="col-xs-4">
               <ul className="sub-list">
-                <li><span className="sub-header">Authored by Dominic Moorman</span></li>
-                <li><a href="http://github.com/pranked">Github</a></li>
+                <li><a href="https://github.com/pranked"><img className="img-circle" src="https://avatars1.githubusercontent.com/u/9093726" alt="" width={size} height={size} /> <span className="sub-header">Created by @pranked</span></a></li>
                 <li><a href="mailto:dom@levee.co ">dom@levee.co</a></li>
               </ul>
             </div>
             <div className="col-xs-4"></div>
             <div className="col-xs-4 text-right">
-              <div className="text-small">Copyright © 2016</div>
-              <span>levee.co</span>
+              <div className="text-small"><a href="https://github.com/pranked/cs2-web/blob/master/LICENSE.md">MIT License</a></div>
+              <span>Levee.co</span>
             </div>
           </div>
         </div>
